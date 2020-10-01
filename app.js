9bd8bbdeb3ce706670dfb84dbda3efba6ac8bc41
Arundas Purayil <arundas0purayil@gmail.com>
2020-10-01 13:07:42 -0400

    Added feature to only return text after mention in a tweet reply

--- app.js
+++ app.js
@@ -6,46 +6,51 @@ client.login(process.env.TOKEN);
 let allTweets = [];
 
 async function getTweets() {
-	let maxId = Infinity;
-	let config = {
-		screen_name: 'Tholl_22',
-		exclude_replies: false,
-		include_rts: false,
-	};
+  let maxId = Infinity;
+  let config = {
+    screen_name: 'Tholl_22',
+    exclude_replies: false,
+    include_rts: false,
+  };
 
-	let tweets = await twitter.get('statuses/user_timeline', config);
+  let tweets = await twitter.get('statuses/user_timeline', config);
 
-	while (tweets.length > 1) {
-		tweets.forEach(tweet => {
-			if (!tweet.hasOwnProperty('quoted_status')) {
-				allTweets.push(tweet.text);
-			}
-			if (tweet.id < maxId) {
-				maxId = tweet.id;
-			}
-		});
+  while (tweets.length > 1) {
+    tweets.forEach(tweet => {
+      //If reply get the text after reply @ mention
 
-		config.max_id = maxId;
-		tweets = await twitter.get('statuses/user_timeline', config);
-	}
+      if (!tweet.hasOwnProperty('quoted_status')) {
+        let text = { tweet };
+        if (tweet.in_reply_to_screen_name) {
+          text = text.substring(text.indexOf(' ') + 1);
+        }
+        allTweets.push(text);
+      }
+
+      if (tweet.id < maxId) {
+        maxId = tweet.id;
+      }
+    });
+
+    config.max_id = maxId;
+    tweets = await twitter.get('statuses/user_timeline', config);
+  }
 }
 
 async function getRandomTweet() {
-	if (allTweets.length == 0) await getTweets();
+  if (allTweets.length == 0) await getTweets();
 
-	const random = Math.floor(Math.random() * allTweets.length);
-	const msg = allTweets[random];
-	allTweets.splice(random, 1);
-	return msg;
+  const random = Math.floor(Math.random() * allTweets.length);
+  const msg = allTweets[random];
+  allTweets.splice(random, 1);
+  return msg;
 }
 
 client.on('message', async msg => {
-	const message = msg.content.toLowerCase();
-	const { username } = msg.author;
-	if (message.includes('tanner') || message.includes('tholl')) {
-		const tweet = await getRandomTweet();
-		msg.channel.send(tweet);
-	} else if (username === 'tholl222') {
-		msg.channel.send('Balls in my face');
-	}
+  const message = msg.content.toLowerCase();
+  const { username } = msg.author;
+  if (message.includes('tanner') || message.includes('tholl')) {
+    const tweet = await getRandomTweet();
+    msg.channel.send(tweet);
+  }
 });
