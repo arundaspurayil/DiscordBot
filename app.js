f845bff39bf9909bdd1e05a41a3a340fa8d80af6
Arun Purayil <arun.purayil@joinroot.com>
2021-07-25 17:26:44 -0400

    WIP

--- app.js
+++ app.js
@@ -1,54 +1,18 @@
 const Twitter = require('twitter');
-const twitter = new Twitter(require('./twitterConfig'));
+const TwitterService = require('./twitter-service');
 const Discord = require('discord.js');
+
+const twitterClient = new Twitter(require('./twitterConfig'));
+const twitterService = new TwitterService(twitterClient, 'tholl_22');
 const client = new Discord.Client();
+
 client.login(process.env.TOKEN);
-let allTweets = [];
-
-async function getTweets() {
-  let maxId = Infinity;
-  let config = {
-    screen_name: 'Tholl_22',
-    exclude_replies: false,
-    include_rts: false,
-  };
-
-  let tweets = await twitter.get('statuses/user_timeline', config);
-
-  while (tweets.length > 1) {
-    tweets.forEach(tweet => {
-      if (!tweet.hasOwnProperty('quoted_status')) {
-        let { text } = tweet;
-
-        //If reply get the text after reply @ mention
-        if (text.charAt(0) == '@') {
-          text = text.substring(text.indexOf(' ') + 1);
-        }
-        allTweets.push(text);
-      }
-
-      maxId = Math.min(tweet.id, maxId);
-    });
-
-    config.max_id = maxId;
-    tweets = await twitter.get('statuses/user_timeline', config);
-  }
-}
-
-async function getRandomTweet() {
-  if (allTweets.length == 0) await getTweets();
-
-  const random = Math.floor(Math.random() * allTweets.length);
-  const msg = allTweets[random];
-  allTweets.splice(random, 1);
-  return msg;
-}
 
 client.on('message', async msg => {
   const message = msg.content.toLowerCase();
-  const { username } = msg.author;
+
   if (message.includes('tanner') || message.includes('tholl')) {
-    const tweet = await getRandomTweet();
+    const tweet = await twitterService.getRandomTweet();
     msg.channel.send(tweet);
   }
 });
---
+++ twitter-service.js
@@ -0,0 +1,44 @@
+module.exports = class TwitterService {
+  constructor(twitterClient, twitterHandle){
+    this.twitterClient = twitterClient;
+    this.twitterHandle = twitterHandle;
+    this.tweets = [];
+  }
+
+  async #getTweets(maxId = Infinity, prevMaxId = undefined) {
+    if(maxId === prevMaxId) { return; }
+
+    let config = {
+      screen_name: this.twitterHandle,
+      exclude_replies: false,
+      include_rts: false,
+      ...(maxId !== Infinity && { max_id: maxId })
+    };
+  
+    let tweets = await this.twitterClient.get('statuses/user_timeline', config);
+    let currentMaxId;
+
+    tweets.forEach(tweet => {
+      if(tweet.hasOwnProperty('quoted_status')) { return; }
+      let { text } = tweet;
+
+      if (text.charAt(0) == '@') {
+        text = text.substring(text.indexOf(' ') + 1);
+      }
+
+      this.tweets.push(text);
+      currentMaxId = Math.min(tweet.id, maxId);
+    });
+
+    await this.#getTweets(currentMaxId, maxId);
+  }
+
+  async getRandomTweet() {
+    if (this.tweets.length == 0) await this.#getTweets();
+
+    const randomNumber = Math.floor(Math.random() * this.tweets.length);
+    const [msg] = this.tweets.splice(randomNumber, 1);
+    return msg;
+  }
+}
+
