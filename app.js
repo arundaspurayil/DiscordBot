5a4add6285bba6b7ac630a7d6ac1e6183fd386ad
Arun Purayil <arun@Aruns-Air.attlocal.net>
2022-04-09 14:37:22 -0400

    Added event listener for deleted messages

--- app.js
+++ app.js
@@ -6,11 +6,24 @@ const client = new Discord.Client();
 
 client.login(process.env.TOKEN);
 
-client.on('message', async msg => {
+client.on('messageCreate', async msg => {
   const message = msg.content.toLowerCase();
 
   if (message.includes('tanner') || message.includes('tholl')) {
     const tweet = await twitterService.getRandomTweet();
     msg.channel.send(tweet);
   }
+});
+
+client.on('messageDelete', async msg => {
+  let message = msg.content;
+  const author = msg.author.username
+  
+  if(msg.attachments){
+    msg.attachments.forEach(attachment => {
+      message = message.concat(attachment.proxyURL)
+    })
+  }
+  
+  msg.channel.send(`${author} deleted message: ${message}`);
 });
