4cc1bb842dc08c5dc3124c243fa00b224f25a83d
Arun Purayil <arun@Aruns-Air.attlocal.net>
2022-04-11 12:44:31 -0400

    Formatted all files

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const TwitterService = require('./twitter-service');
 const Discord = require('discord.js');
-const cron = require('node-cron')
-const MessageCounter = require("./message-counter")
+const cron = require('node-cron');
+const MessageCounter = require('./message-counter');
 
 const twitterService = new TwitterService('tholl_22');
 const client = new Discord.Client();
 
 client.login(process.env.TOKEN);
 
-client.on('message', async msg => {
-  if(msg.author.id === client.user.id) { return; }
-  MessageCounter.incrementCounter(msg.author?.username)
+client.on('message', async (msg) => {
+  if (msg.author.id === client.user.id) {
+    return;
+  }
+  MessageCounter.incrementCounter(msg.author?.username);
 
   const message = msg.content.toLowerCase();
 
@@ -20,44 +22,54 @@ client.on('message', async msg => {
   }
 });
 
-client.on('messageDelete', async msg => {
+client.on('messageDelete', async (msg) => {
   let message = msg.content;
-  const author = msg.author.username
-  
-  if(msg.attachments){
-    msg.attachments.forEach(attachment => {
-      message = message.concat(attachment.proxyURL)
-    })
+  const author = msg.author.username;
+
+  if (msg.attachments) {
+    msg.attachments.forEach((attachment) => {
+      message = message.concat(attachment.proxyURL);
+    });
   }
-  
+
   msg.channel.send(`${author} deleted message: ${message}`);
 });
 
-
 client.on('ready', () => {
-  scheduleCronJob("0 0 9 * * *", () => sendMessageInAllChannels("Good morning!"))
-  scheduleCronJob("0 0 12 * * *", () => sendMessageInAllChannels("I need chips :)"))
-  scheduleCronJob("0 0 17 * * *", () => sendMessageInAllChannels("Time for dinner!"))
-  scheduleCronJob("0 0 17 * * *", () => {
-    sendMessageInAllChannels(MessageCounter.toString())
-    MessageCounter.clearCounter()
-  })
+  scheduleCronJob('0 0 9 * * *', () =>
+    sendMessageInAllChannels('Good morning!')
+  );
+  scheduleCronJob('0 0 12 * * *', () =>
+    sendMessageInAllChannels('I need chips :)')
+  );
+  scheduleCronJob('0 0 17 * * *', () =>
+    sendMessageInAllChannels('Time for dinner!')
+  );
+  scheduleCronJob('0 0 17 * * *', () => {
+    sendMessageInAllChannels(MessageCounter.toString());
+    MessageCounter.clearCounter();
+  });
 });
 
 const sendMessageInAllChannels = (message) => {
-  const channels = client.channels.cache.filter(c => c.guild && c.type === 'text');
+  const channels = client.channels.cache.filter(
+    (c) => c.guild && c.type === 'text'
+  );
 
-  channels.forEach(async c => {
-    const permissions = await c.permissionsFor(client.user)
-    const hasPermissions = permissions.has(['SEND_MESSAGES', 'VIEW_CHANNEL'], true)
+  channels.forEach(async (c) => {
+    const permissions = await c.permissionsFor(client.user);
+    const hasPermissions = permissions.has(
+      ['SEND_MESSAGES', 'VIEW_CHANNEL'],
+      true
+    );
 
-    if(hasPermissions) c.send(message)
-  })
-}
+    if (hasPermissions) c.send(message);
+  });
+};
 
 const scheduleCronJob = (expression, cb) => {
   cron.schedule(expression, cb, {
     scheduled: true,
-    timezone: "America/New_York"
-  })
-}
+    timezone: 'America/New_York',
+  });
+};
--- message-counter.js
+++ message-counter.js
@@ -1,23 +1,25 @@
 module.exports = {
-    counter: {},
-    incrementCounter: function(author){
-        if(!this.counter.hasOwnProperty(author)){
-            this.counter[author] = 0
-        }
-        
-        this.counter[author] += 1
-    },
-    clearCounter: function(){
-        this.counter = {}
-    },
-    toString: function(){
-        let string = "Message Counter:"
-
-        const keys = Object.keys(this.counter).sort(function(a, b) { return this.counter[a] - this.counter[b] });
-        keys.forEach(key => {
-            string = string.concat("\n", `${key}: ${this.counter[key]}`)
-        })
-
-        return string
+  counter: {},
+  incrementCounter: function (author) {
+    if (!this.counter.hasOwnProperty(author)) {
+      this.counter[author] = 0;
     }
-}
+
+    this.counter[author] += 1;
+  },
+  clearCounter: function () {
+    this.counter = {};
+  },
+  toString: function () {
+    let string = 'Message Counter:';
+
+    const keys = Object.keys(this.counter).sort(function (a, b) {
+      return this.counter[a] - this.counter[b];
+    });
+    keys.forEach((key) => {
+      string = string.concat('\n', `${key}: ${this.counter[key]}`);
+    });
+
+    return string;
+  },
+};
