1f300dcff4868c079165b35fdc83a979c2961c36
Arun Purayil <arun@Aruns-MacBook-Air.local>
2022-04-11 22:03:45 -0400

    WIP

--- message-counter.js
+++ message-counter.js
@@ -1,26 +1,21 @@
 module.exports = {
   counter: {},
-  incrementCounter: function (author) {
+  incrementCounter(author) {
     if (!this.counter.hasOwnProperty(author)) {
       this.counter[author] = 0;
     }
 
     this.counter[author] += 1;
   },
-  clearCounter: function () {
+  clearCounter() {
     this.counter = {};
   },
-  toString: function () {
-    const sortFunction = function (a, b) {
-      return this.counter[b] - this.counter[a];
-    }.bind(this);
-
+  toString() {
     let string = 'Message Counter:';
 
-    const keys = Object.keys(this.counter).sort(sortFunction);
-    keys.forEach((key) => {
-      string = string.concat('\n', `${key}: ${this.counter[key]}`);
-    });
+    Object.keys(this.counter)
+      .sort((a, b) => this.counter[b] - this.counter[a])
+      .forEach((key) => (string += '\n' + `${key}: ${this.counter[key]}`));
 
     return string;
   },
