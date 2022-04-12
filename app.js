8456626d556182063eb223ec2b367023e0130f11
Arun Purayil <arun@Aruns-MacBook-Air.local>
2022-04-11 20:04:49 -0400

    WIP

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ client.on('ready', () => {
   scheduleCronJob('0 0 17 * * *', () =>
     sendMessageInAllChannels('Time for dinner!')
   );
-  scheduleCronJob('0 0 19 * * *', () => {
+  scheduleCronJob('0 0 22 * * *', () => {
     sendMessageInAllChannels(MessageCounter.toString());
     MessageCounter.clearCounter();
   });
