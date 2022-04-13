a2e0dfe222b7c21ef0614e721f0916c67c84968d
Arun Purayil <arun@Aruns-MacBook-Air.local>
2022-04-13 10:47:37 -0400

    Update task to run every sunday

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ client.on('ready', () => {
   scheduleCronJob('0 0 17 * * *', () =>
     sendMessageInAllChannels('Time for dinner!')
   );
-  scheduleCronJob('0 0 22 * * *', () => {
+  scheduleCronJob('0 0 22 * * Sunday', () => {
     sendMessageInAllChannels(MessageCounter.toString());
     MessageCounter.clearCounter();
   });
