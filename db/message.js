d6325bf1a77e41a4044cf1867e7756bef131cc3e
Arun Purayil <arundas0purayil@gmail.com>
2022-04-16 20:42:26 -0400

    Refactored message counter to messages

---
+++ db/message.js
@@ -0,0 +1,42 @@
+const moment = require("moment");
+const fs = require("fs");
+const path = require("path");
+
+const filePath = path.join(__dirname, "messages.json");
+
+const proxyHandler = {
+  set: function (obj, prop, value) {
+    const res = Reflect.set(obj, prop, value);
+
+    persistMessagesToFile(obj);
+
+    return res;
+  },
+};
+
+module.exports = {
+  messages: new Proxy(readMessagesFromFile(), proxyHandler),
+  create(author) {
+    this.messages.push({
+      author,
+      createdAt: moment(),
+    });
+  },
+  findFromStartDate(startDate) {
+    return this.messages.filter((message) =>
+      moment(message.createdAt).isBefore(startDate)
+    );
+  },
+};
+
+function persistMessagesToFile(messages) {
+  fs.writeFileSync(filePath, JSON.stringify(messages));
+}
+
+function readMessagesFromFile() {
+  if (!fs.existsSync(filePath)) {
+    return [];
+  }
+
+  return JSON.parse(fs.readFileSync(filePath));
+}
