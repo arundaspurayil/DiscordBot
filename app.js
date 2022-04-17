f5ff63228defdb19eb12072dc0713d2f173cfa5d
arundas0purayil@gmail.com <arundas0purayil@gmail.com>
2022-04-17 12:20:40 -0400

    Updated app to use utils and mongodb

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const TwitterService = require('./twitter-service');
 const Discord = require('discord.js');
-const cron = require('node-cron');
-const MessageCounter = require('./message-counter');
+const MongoClient = require('./db/mongo-client');
+const Messages = require('./db/messages');
+const { scheduleCronJob, sendMessageInAllChannels } = require('./lib/utils');
 
 const twitterService = new TwitterService('tholl_22');
 const client = new Discord.Client();
 
-client.login(process.env.TOKEN);
+MongoClient.connect().then(() => {
+  client.login(process.env.TOKEN);
+});
 
 client.on('message', async (msg) => {
   if (msg.author.id === client.user.id) {
     return;
   }
-  MessageCounter.incrementCounter(msg.author?.username);
+  Messages.createMessage(msg.author?.username, msg.content);
 
   const message = msg.content.toLowerCase();
 
@@ -22,51 +25,20 @@ client.on('message', async (msg) => {
   }
 });
 
-client.on('messageDelete', async (msg) => {
-  if (process.env.SEND_DELETED_MESSAGE !== 'true') {
-    return;
-  }
-  let message = msg.content;
-  const author = msg.author.username;
-
-  if (msg.attachments) {
-    msg.attachments.forEach((attachment) => {
-      message = message.concat(attachment.proxyURL);
-    });
-  }
-
-  msg.channel.send(`${author} deleted message: ${message}`);
-});
-
 client.on('ready', () => {
   scheduleCronJob('0 0 17 * * *', () =>
-    sendMessageInAllChannels('Time for dinner!')
+    sendMessageInAllChannels(client, 'Time for dinner!')
   );
   scheduleCronJob('0 0 22 * * Sunday', () => {
-    sendMessageInAllChannels(MessageCounter.toString());
+    sendMessageInAllChannels(client, MessageCounter.toString());
     MessageCounter.clearCounter();
   });
 });
 
-const sendMessageInAllChannels = (message) => {
-  const channels = client.channels.cache.filter(
-    (c) => c.guild && c.type === 'text'
-  );
-
-  channels.forEach(async (c) => {
-    const permissions = await c.permissionsFor(client.user);
-    const hasPermissions = permissions.has(
-      ['SEND_MESSAGES', 'VIEW_CHANNEL'],
-      true
-    );
-
-    if (hasPermissions) c.send(message);
-  });
+const cleanup = async (event) => {
+  await MongoClient.close();
+  process.exit();
 };
 
-const scheduleCronJob = (expression, cb) => {
-  cron.schedule(expression, cb, {
-    scheduled: true,
-    timezone: 'America/New_York',
-  });
-};
+process.on('SIGINT', cleanup);
+process.on('SIGTERM', cleanup);
