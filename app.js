10c14eef7ba1462405c5a6e62528cf7e24315a8c
Arun Purayil <37260317+arundaspurayil@users.noreply.github.com>
2022-08-21 13:31:42 -0400

    Remove message counter and server (#18)

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
 const TwitterService = require('./twitter-service');
 const Discord = require('discord.js');
-const MongoClient = require('./db/mongo-client');
-const Messages = require('./db/messages');
 const { scheduleCronJob, sendMessageInAllChannels } = require('./lib/utils');
 
 const twitterService = new TwitterService('tholl_22');
 const client = new Discord.Client();
 
-MongoClient.connect().then(() => {
-  client.login(process.env.TOKEN);
-});
-
 client.on('message', async (msg) => {
   if (msg.author.id === client.user.id) {
     return;
   }
-  Messages.createMessage(msg.author?.username, msg.content);
 
   const message = msg.content.toLowerCase();
 
@@ -29,16 +22,4 @@ client.on('ready', () => {
   scheduleCronJob('0 0 17 * * *', () =>
     sendMessageInAllChannels(client, 'Time for dinner!')
   );
-  scheduleCronJob('0 0 22 * * Sunday', () => {
-    sendMessageInAllChannels(client, MessageCounter.toString());
-    MessageCounter.clearCounter();
-  });
 });
-
-const cleanup = async (event) => {
-  await MongoClient.close();
-  process.exit();
-};
-
-process.on('SIGINT', cleanup);
-process.on('SIGTERM', cleanup);
